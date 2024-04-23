37fbc15d0dd051cb913ae74c543c384ee83dba40
Quan Do <minhquand3@gmail.com>
2024-04-22 21:49:05 -0700

    Solution difference_of_squares

--- c/difference-of-squares/difference_of_squares.c
+++ c/difference-of-squares/difference_of_squares.c
@@ -1,1 +1,15 @@
 #include "difference_of_squares.h"
+
+unsigned int square_of_sum(unsigned int number) {
+    unsigned int sum = ((number + 1) * number) / 2;
+    return sum * sum;
+}
+
+unsigned int sum_of_squares(unsigned int number) {
+    unsigned int result = (number * (number + 1) * ((number * 2) + 1)) / 6;
+    return result;
+}
+
+unsigned int difference_of_squares(unsigned int number) {
+    return square_of_sum(number) - sum_of_squares(number);
+}
