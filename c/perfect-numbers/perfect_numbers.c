be4bf4de93eb52329c52c3268e16eb2c3db840ff
Quan Do <minhquand3@gmail.com>
2024-05-07 16:19:58 -0700

    Solution perfect_numbers

---
+++ c/perfect-numbers/perfect_numbers.c
@@ -0,0 +1,22 @@
+#include "perfect_numbers.h"
+
+kind classify_number(int num) {
+    if (num <= 0) {
+        return ERROR;
+    }
+
+    int sum = 0;
+    for (int i = 1; i <= num / 2; i++) {
+        if (num % i == 0) {
+            sum += i;
+        }
+    }
+
+    if (sum == num) {
+        return PERFECT_NUMBER;
+    } else if (sum > num) {
+        return ABUNDANT_NUMBER;
+    } else {
+        return DEFICIENT_NUMBER;
+    }
+}
