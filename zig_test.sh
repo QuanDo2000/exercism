fbd2ce2b1914a1629fe4096c910cd89adc1965a6
Quan Do <minhquand3@gmail.com>
2024-06-30 18:25:22 -0700

    Update zig_test

--- zig_test.sh
+++ zig_test.sh
@@ -1,4 +1,7 @@
-for file in "./zig/*/test_*.zig"; do
+for subdir in ./zig/*/; do
+    cd $subdir
+    file=`find . -name 'test_*.zig'`
     echo `basename $file`
     zig test $file
+    cd ../..
 done
