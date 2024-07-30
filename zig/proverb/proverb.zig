3514823c00c4dcdf9ac7a10c22852d153aa58499
Quan Do <minhquand3@gmail.com>
2024-07-30 10:49:18 -0700

    Solution proverb.zig

---
+++ zig/proverb/proverb.zig
@@ -0,0 +1,16 @@
+const std = @import("std");
+const mem = std.mem;
+const fmt = std.fmt;
+
+pub fn recite(allocator: mem.Allocator, words: []const []const u8) (fmt.AllocPrintError || mem.Allocator.Error)![][]u8 {
+    var ans: [][]u8 = try allocator.alloc([]u8, words.len);
+    if (words.len > 1) {
+        for (words[1..], 1..) |word, i| {
+            ans[i - 1] = try fmt.allocPrint(allocator, "For want of a {s} the {s} was lost.\n", .{ words[i - 1], word });
+        }
+    }
+    if (words.len > 0) {
+        ans[words.len - 1] = try fmt.allocPrint(allocator, "And all for the want of a {s}.\n", .{words[0]});
+    }
+    return ans;
+}
